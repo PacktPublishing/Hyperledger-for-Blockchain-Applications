7af1302ec2fb00e004ed149670363adbcfb5b913
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2018-06-19 20:48:19 +0300

    Add Transaction that transfers landTitle from one Individual to another
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- section_4/landreg/lib/logic.js
+++ section_4/landreg/lib/logic.js
@@ -25,3 +25,43 @@ async function unlockLandTitle(tx) {
     // Update the asset in the asset registry.
     await landTitleRegistry.update(tx.landTitle);
 }
+
+/**
+ * Transfer land title from one owner to another
+ * @param {org.landreg.TransferLandTitle} tx - The transaction instance
+ * @transaction
+ */
+async function transferLandTitle(tx) {
+    // Get asset registeries for landTitles and Individuals
+    const landTitleRegistry = await getAssetRegistry(NS + '.LandTitle');
+    const individualRegistry = await getParticipantRegistry(NS + '.Individual');
+
+    // Check if the landTitle is not for sale
+    if (!tx.landTitle.forSale) {
+        throw new Error(`Land Title with id ${tx.landTitle.getIdentifier()} is not marked for sale`);
+    }
+
+    // Get newOwner
+    const newOwnerID = tx.newOwner.getIdentifier();
+    const oldOwnerID = tx.landTitle.owner.getIdentifier();
+
+    // Make sure that new owner exists
+    const newOwner = await individualRegistry.get(newOwnerID);
+
+    // Check that newOwner is not same as current owner
+    if (newOwnerID == oldOwnerID) {
+        throw new Error(`Land Title with id ${tx.landTitle.getIdentifier()} is already owned by ${oldOwnerID}`);
+    }
+
+    // Add current landTitle owner to previousOwners array
+    tx.landTitle.previousOwners.push(tx.landTitle.owner);
+
+    // Update land title with new owner
+    tx.landTitle.owner = tx.newOwner;
+
+    // Set land title as not for sale anymore
+    tx.landTitle.forSale = false;
+
+    // Update the asset in the asset registry.
+    await landTitleRegistry.update(tx.landTitle);
+}
