6f5f6c79dec7e058cb89195c5a2fcfce66ea0b77
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2018-07-08 23:23:22 +0300

    Add placeholders for tests

--- section_6/landreg/test/test.js
+++ section_6/landreg/test/test.js
@@ -18,3 +18,37 @@ const adminUserName = 'admin';
 const adminEnrollmentSecret = 'adminpw';
 
 const fixtures = yaml.safeLoad(fs.readFileSync(__dirname + '/fixtures.yml', 'utf8'));
+
+describe('Unit tests', () => {
+
+    describe('Individual', () => {
+
+        it('should create several individuals');
+    });
+
+    describe('LandTitle', () => {
+
+        it('should create several landTitles');
+
+        describe('unlockLandTitle', () => {
+
+            it('should unlock locked landTitle');
+
+            it('should not unlock unlocked landTitle');
+        });
+
+        describe('transferLandTitle', () => {
+
+            it('should not transfer landTitle when newOwner is equal to current owner');
+
+            it('should transfer unlocked landTitle');
+
+            it('should not transfer locked landTitle');
+        });
+    });
+
+    describe('Query', () => {
+
+        it('should list ListLandTitlesBySize, and correctly filter by maximumArea');
+    });
+});
